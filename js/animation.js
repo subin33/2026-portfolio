00597cc65a5acdde265bd64dc24af26763578c65
subin33 <shtnqls33@gmail.com>
2026-01-15 10:45:15 +0900

    feat: add garfield section layout and animation scaffold

---
+++ js/animation.js
@@ -0,0 +1,13 @@
+// Define DOM elements
+const garfieldImage = document.querySelector("garfield__animation__img");
+
+// Store last reverse animation, ready to be played
+let lastReverseAni = "";
+
+function playAnimation(ani, reversAni) {
+  // Remove all the animation classes from garfieldImage
+  garfieldImage.className = "";
+
+  if (lastReverseAni !== "") {
+  }
+}
