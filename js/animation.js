1138f943fdba5d0f980e74185460c476cdf6420e
subin33 <shtnqls33@gmail.com>
2026-01-16 14:04:36 +0900

    feat: add corner button interaction and active state handling

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,19 @@
 // Define DOM elements
 const garfieldImage = document.querySelector("garfield__animation__img");
 
+const tlBtn = document.querySelector("#corner__tl__btn");
+const trBtn = document.querySelector("#corner__tr__btn");
+const blBtn = document.querySelector("#corner__bl__btn");
+const brBtn = document.querySelector("#corner__br__btn");
+
+const tlContent = document.querySelector("#corner__tl__content");
+const trContent = document.querySelector("#corner__tr__content");
+const blContent = document.querySelector("#corner__bl__content");
+const brContent = document.querySelector("#corner__br__content");
+
+// Define corner that is open
+let activeCorner = "";
+
 // Store last reverse animation, ready to be played
 let lastReverseAni = "";
 
@@ -11,3 +24,14 @@ function playAnimation(ani, reversAni) {
   if (lastReverseAni !== "") {
   }
 }
+
+// Onclick corner button functions
+tlBtn.onclick = function () {
+  trBtn.innerHTML = "Experience";
+  blBtn.innerHTML = "Projects";
+  brBtn.innerHTML = "Contact";
+
+  // Setting activeCorner
+  activeCorner = "top-left";
+  tlBtn.innerHTML = "&uarr;<br/>About";
+};
