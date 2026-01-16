83f0cbe3b06265f3e129d61cd36e73c46a03d965
subin33 <shtnqls33@gmail.com>
2026-01-16 14:42:19 +0900

    feat: add corner-based UI state handling and face animation sequences

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 // Define DOM elements
-const garfieldImage = document.querySelector("garfield__animation__img");
+const garfieldImage = document.querySelector("#garfield__animation__img");
 
 const tlBtn = document.querySelector("#corner__tl__btn");
 const trBtn = document.querySelector("#corner__tr__btn");
@@ -11,17 +11,44 @@ const trContent = document.querySelector("#corner__tr__content");
 const blContent = document.querySelector("#corner__bl__content");
 const brContent = document.querySelector("#corner__br__content");
 
+// Define colors and positions
+const bgColor = "#fff";
+const bgColorAlt = "#333";
+const textColor = "#333";
+const textColorAlt = "#fff";
+
+let tlActive = "translateX(5vw) translateY(0)";
+let tlHidden = "translateX(-100vw) translateY(-100vh)";
+
+let trActive = "translateX(-5vw) translateY(0)";
+let trHidden = "translateX(100vw) translateY(-100vh)";
+
+let blActive = "translateX(10vw) translateY(7vh)";
+let blHidden = "translateX(-100vw) translateY(100vh)";
+
+let brActive = "translateX(-5vw) translateY(0)";
+let brHidden = "translateX(100vw) translateY(100vh)";
+
 // Define corner that is open
 let activeCorner = "";
 
 // Store last reverse animation, ready to be played
-let lastReverseAni = "";
+let lastReverseAnimation = "";
 
-function playAnimation(ani, reversAni) {
+function playAnimation(animation, reversAnimation) {
   // Remove all the animation classes from garfieldImage
   garfieldImage.className = "";
 
-  if (lastReverseAni !== "") {
+  if (lastReverseAnimation !== "") {
+    garfieldImage.classList.add(lastReverseAnimation);
+    setTimeout(() => {
+      garfieldImage.classList.remove(lastReverseAnimation);
+      garfieldImage.classList.add(animation);
+      lastReverseAnimation = reversAnimation;
+    }, 200);
+  } else {
+    garfieldImage.classList.add(animation);
+    lastReverseAnimation = reversAnimation;
   }
 }
 
@@ -34,4 +61,24 @@ tlBtn.onclick = function () {
   // Setting activeCorner
   activeCorner = "top-left";
   tlBtn.innerHTML = "&uarr;<br/>About";
+
+  playAnimation("animate-top-left", "reverse-animate-top-left");
+
+  // Change background colors
+  trBtn.style.background = bgColor;
+  brBtn.style.background = bgColor;
+  blBtn.style.background = bgColor;
+  tlBtn.style.background = bgColorAlt;
+
+  // Change text colors
+  trBtn.style.color = textColor;
+  brBtn.style.color = textColor;
+  blBtn.style.color = textColor;
+  tlBtn.style.color = textColorAlt;
+
+  // Change positions of the corner content
+  trContent.style.transform = trHidden;
+  brContent.style.transform = brHidden;
+  blContent.style.transform = blHidden;
+  tlContent.style.transform = tlActive;
 };
